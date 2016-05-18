9d27a6d5e2b0846d68337145920286faaa4deb23
Sunny Garg <sunny.shiva@gmail.com>
2016-05-18 17:51:41 +0530

    Add new and create actions with view files

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,4 +2,18 @@ class StoriesController < ApplicationController
 
 	def index
 	end
+
+	def new
+		@story = Story.new
+	end
+
+	def create
+		@story = Story.new(story_params)
+	end
+
+	private
+
+	def story_params
+		params.require(:story).permit(:title, :description, :creator)
+	end
 end
