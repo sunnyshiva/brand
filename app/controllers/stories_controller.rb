8f458075c9ff70889db919002b45ae0e1f38241c
Sunny Garg <sunny.shiva@gmail.com>
2016-05-20 23:34:08 +0530

    Edit and delete actions and view files

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -22,6 +22,22 @@ class StoriesController < ApplicationController
 		end
 	end
 
+	def edit
+	end
+
+	def update
+		if @story.update(story_params)
+			redirect_to story_path(@story)
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@story.destroy
+		redirect_to root_path
+	end
+
 	private
 
 	def story_params
