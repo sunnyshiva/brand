52ded82318eabd42a3b19fa35e6533c374b1fb71
Sunny Garg <sunny.shiva@gmail.com>
2016-05-22 01:32:07 +0530

    Assign category to stories

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -9,11 +9,13 @@ class StoriesController < ApplicationController
 	end
 
 	def new
-		@story = current_user.story.build
+		@story = current_user.stories.build
+		@categories = Category.all.map{ |c| [c.name, c.id] }
 	end
 
 	def create
-		@story = current_user.story.build(story_params)
+		@story = current_user.stories.build(story_params)
+		@story.category_id = params[:category_id]
 
 		if @story.save
 			redirect_to root_path
@@ -23,9 +25,12 @@ class StoriesController < ApplicationController
 	end
 
 	def edit
+		@categories = Category.all.map{ |c| [c.name, c.id] }
 	end
 
 	def update
+		@story.category_id = params[:category_id]
+
 		if @story.update(story_params)
 			redirect_to story_path(@story)
 		else
@@ -40,12 +45,12 @@ class StoriesController < ApplicationController
 
 	private
 
-	def story_params
-		params.require(:story).permit(:title, :description, :creator)
-	end
+		def story_params
+			params.require(:story).permit(:title, :description, :creator, :category_id)
+		end
 
-	def find_story
-		@story = Story.find(params[:id])
-	end
+		def find_story
+			@story = Story.find(params[:id])
+		end
 
 end
