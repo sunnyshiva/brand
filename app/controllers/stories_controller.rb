09edba7085f7e6cf75e63a76927393e38870367d
Sunny Garg <sunny.shiva@gmail.com>
2016-05-23 03:12:44 +0530

    Add average review and authentication

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,5 +1,6 @@
 class StoriesController < ApplicationController
 	before_action :find_story, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!, only: [:new, :edit]
 
 	def index
 		if params[:category].blank?
@@ -11,6 +12,11 @@ class StoriesController < ApplicationController
 	end
 
 	def show
+		if @story.reviews.blank?
+			@average_review = 0
+		else
+			@average_review = @story.reviews.average(:rating).round(2)
+		end
 	end
 
 	def new
